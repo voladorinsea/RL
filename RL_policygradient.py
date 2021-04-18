fe8c0a09f167beab02534f33988c6618f41729f9
wings <1195372303@qq.com>
2021-04-18 22:02:15 +0800

    Update RL_policygradient.py

--- RL_policygradient.py
+++ RL_policygradient.py
@@ -1,5 +1,7 @@
 import gym
 import PID
+import numpy as np
+import random
 class env():
     def __init__(self,end):
         self.time=0
@@ -36,7 +38,7 @@ class agent():
     def execute(self):
         action=self.value_network.policysearch(self.state)
         self.state, reward, self.check, self.last_state=self.env.episode_action(action)
-        self.value_network.gradient(self.state, reward, self.last_state)
+        self.value_network.gradient(self.state, reward, self.last_state,action)
     def training(self):
         cycle=0
         while(cycle!=self.cycle):
@@ -51,24 +53,78 @@ class agent():
 
 
 class value_network():
-    def __init__(self):
-        self.learning_rate=0.1
+    def __init__(self,learning_rate,gama,epsilon):
+        self.learning_rate=learning_rate
+        self.w=np.array([1,1,1,1,1,1])
+        self.gama=gama
+        self.epsilon=epsilon
+        '''
         self.Pidout=PID.PIDcontrol(0.3,1000,5,0,0)
         self.Pidin=PID.PIDcontrol(0.5,1000,0,50,0)
+        '''
     def policysearch(self,state):
+        # PID for a test
+        '''
         out1=self.Pidout.control(state[1])
         self.Pidin.modify(out1)
         out2=self.Pidin.control(state[3])
         action = 1 if out2<=50 else 0
-        return action
-    def gradient(self,state,reward,last_state):
-        return 0
+        '''
+        # based on epsilon-greedy policy
+        value0=self.state_action_valuesearch(state,0)
+        value1=self.state_action_valuesearch(state,1)
 
+        gen1=random.random()
+        if gen1>=self.epsilon:
+            if value0>=value1:
+                return 0
+            else:
+                return 1
+        else:
+            gen2=random.random()
+            if gen2>0.5:
+                return 0
+            else:
+                return 1
+    def gradient(self,state,reward,last_state,action):
+        next_value=self.state_valuesearch(state)
+        sample_value=reward+self.gama*next_value
+        evaluate_value=self.state_valuesearch(last_state)
+
+        last_state=np.array(last_state)
+
+        action=np.array(self.action_to_onehot(action))
+        state_action=np.hstack([state,action]).T
+        delta_w=self.learning_rate*(evaluate_value-sample_value)*state_action
+        self.w=self.w+delta_w
+        print(self.w.T)
+    def state_action_valuesearch(self,state,action):
+        state=np.array(state)
+        action=np.array(self.action_to_onehot(action))
+        state_action=np.hstack([state,action]).T
+        return np.matmul(self.w.T,state_action)
+    def state_valuesearch(self,state):
+        # based on best policy principle
+        value0=self.state_action_valuesearch(state,0)
+        value1=self.state_action_valuesearch(state,1)
+        # print(value0)
+        if value0>=value1:
+            return value0
+        else:
+            return value1
+    def action_to_onehot(self,action):
+        if action==0:
+            return [0,1]
+        else:
+            return [1,0]
 
 if __name__=="__main__":
-    goal=500
-    trainset=20
+    goal=200
+    trainset=2000
+    learning_rate=0.01
+    gama=0.5
+    epsilon=0.5
     AI_env=env(goal)
-    model=value_network()
+    model=value_network(learning_rate,gama,epsilon)
     AI=agent(trainset,AI_env,model,goal)
     AI.training()
